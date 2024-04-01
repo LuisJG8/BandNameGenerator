f6778d5983edd18d5cafc61fc8062a61dd92863a
Luis Gonzalez <162064187+LuisJG8@users.noreply.github.com>
2024-04-01 15:29:14 -0400

    Add files via upload

---
+++ bandName.py
@@ -0,0 +1,38 @@
+print("Welcome to the Band Name Generator")
+city = input("What's the name of the city you grew up in?\n")
+petsName = input("What's your pet's name\n")
+print(f"Your band name could be {city} {petsName}")
+
+# I tried to make the code harder by using loops to check if the user's input was anything besides what I was asking,
+# this is my version with loops that had a structure error because I did not include the inputs inside the while loop
+
+# print("Welcome to the Band Name Generator")
+# city = input("What's the name of the city you grew up in?\n")
+# petsName = input("What's your pet's name\n")
+#
+# flag = True
+#
+# while flag == True and city != str and petsName != str:
+#     print("Insert valid names")
+#     flag = False
+# if city == str and petsName == str:
+#         print("Welcome to the Band Name Generator")
+#         city = str(input("What's the name of the city you grew up in?\n"))
+#         petsName = str(input("What's your pet's name\n"))
+#         print(f"Your band name could be {city} {petsName}")
+
+
+# this is ChatGpt Improved Version
+
+# print("Welcome to the Band Name Generator")
+# flag = True
+#
+# while flag:
+#     city = input("What's the name of the city you grew up in?\n")
+#     petsName = input("What's your pet's name\n")
+#
+#     if city != str or petsName != str:
+#         print("Insert valid names")
+#     else:
+#         print(f"Your band name could be {city} {petsName}")
+#         flag = False
